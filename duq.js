f9b258de0f658aef8be5f077bb1822bf5df2c64b
Tianyi Ma <tn.ma@outlook.com>
2019-02-09 10:45:45 -0600

    matrix op generation

--- duq.js
+++ duq.js
@@ -1,81 +1,46 @@
-class Duq {
-
-  constructor() {
-    this.qReg = [];
-    this.cReg = [];
-  }
-
-}
-
-class Qubit {
-
-  constructor(u, d) {
-    this.u = u;
-    this.d = d;
-  }
-
-  collapse() {
-    return Math.random() < this.probability();
-  }
-
-  probability() {
-    return math.multiply(this.d, this.d);
-  }
-
-  asMatrix() {
-    return math.matrix([this.u, this.d]);
-  }
-
-  toString() {
-    return `(${math.number(this.u)} ${math.number(this.d)})`;
-  }
-
-}
-
 math.config({
   number: 'BigNumber',
   precision: 64
 });
 
-var UTILS = {};
-UTILS.tensorProduct = (matrix1, matrix2) => {
-  var product = [];
-  for (var x = 0; x < matrix1.size()[0]; x++) {
-    for (var y = 0; y < matrix2.size()[0]; y++) {
-      product[(x * matrix1.size()[0]) + y] = math.multiply(matrix1._data[x], matrix2._data[y]);
-    }
-  }
-  return math.matrix(product);
-};
-UTILS.tensorFactor = (matrix1) => {
-
-};
-
 var GATES = {};
 // Hadamard
-GATES.H = (qubit1) => {
-  var hadamardMatrix = math.matrix([
-    [math.divide(math.bignumber(1), math.eval("sqrt(2)")), math.divide(math.bignumber(1), math.eval("sqrt(2)"))],
-    [math.divide(math.bignumber(1), math.eval("sqrt(2)")), math.divide(math.bignumber(-1), math.eval("sqrt(2)"))]
-  ]);
-  var result = math.multiply(hadamardMatrix, qubit1.asMatrix());
-  return {
-    qubit1: new Qubit(result._data[0], result._data[1])
-  };
-};
+GATES.H = math.matrix([
+  [math.eval('1 / sqrt(2)'), math.eval('1 / sqrt(2)')],
+  [math.eval('1 / sqrt(2)'), math.eval('-1 / sqrt(2)')]
+]);
 
 // Controlled X
-GATES.cX => (qubit1, qubit2) {
-  var cXMatrix = math.matrix([
-    [math.bignumber(1), math.bignumber(0), math.bignumber(0), math.bignumber(0)],
-    [math.bignumber(0), math.bignumber(1), math.bignumber(0), math.bignumber(0)],
-    [math.bignumber(0), math.bignumber(0), math.bignumber(0), math.bignumber(1)],
-    [math.bignumber(0), math.bignumber(0), math.bignumber(1), math.bignumber(0)]
-  ]);
-  var tensorProduct = UTILS.tensorProduct(qubit1, qubit2);
-  var tensorFactor = UTILS.tensorFactor(math.multiply(tensorProduct, cXMatrix));
-  return {
-    qubit1: new Qubit(tensorFactor[0]._data[0], tensorFactor[0]._data[1]),
-    qubit2: new Qubit(tensorFactor[1]._data[0], tensorFactor[1]._data[1])
-  };
+GATES.cX = math.matrix([
+  [math.bignumber(1), math.bignumber(0), math.bignumber(0), math.bignumber(0)],
+  [math.bignumber(0), math.bignumber(1), math.bignumber(0), math.bignumber(0)],
+  [math.bignumber(0), math.bignumber(0), math.bignumber(0), math.bignumber(1)],
+  [math.bignumber(0), math.bignumber(0), math.bignumber(1), math.bignumber(0)]
+]);
+
+// X Rotate
+GATES.X = math.matrix([
+  [math.bignumber(0), math.bignumber(1)],
+  [math.bignumber(1), math.bignumber(0)]
+]);
+
+GATES.ID = math.identity(2);
+
+var Duq = {};
+Duq.buildStepOperatorMatrix = (operatorArray) => {
+  if (GATES[operatorArray[0]].size() == 4) {
+    return GATES[operatorArray[0]];
+  }
+  else {
+    return math.kron(GATES[operatorArray[0]], GATES[operatorArray[1]]);
+  }
+};
+Duq.printMatrix = (matrix) => {
+  for (var c = 0; c < matrix._data[0].length; c++) {
+    var line = '';
+    for (var r = 0; r < matrix._data.length; r++) {
+      line += `${math.number(matrix._data[r][c])} `;
+    }
+    console.log(`${line}\n`);
+  }
 };
